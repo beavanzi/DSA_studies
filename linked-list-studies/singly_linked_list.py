a957852f58adb4c79557c1ba53ef4e1770aa0a35
Beatriz Avanzi <beeayee@MacBook-Pro-de-Beatriz.local>
2024-11-30 14:13:44 -0300

    linked list

--- linked-list-studies/singly_linked_list.py
+++ linked-list-studies/singly_linked_list.py
@@ -5,22 +5,39 @@ class ListNode:
 
 
 # T = O(n), S = O(1)
-def traverse(head: ListNode):
+def traverse(node: ListNode):
     
-    # head != None
-    while head:
-        head = head.next
+    # node != None
+    while node:
+        node = node.next
 
 # T = O(1), S = O(1)
 def insert_at_head(head: ListNode, val: int) -> ListNode:
-    new_node = ListNode()
-    new_node.val = val
+    new_node = ListNode(val)
+
     new_node.next = head
     return new_node
 
 # T = O(n), S = O(1)
 def insert_at_tail(head: ListNode, val: int) -> ListNode:
-    pass
+    new_node = ListNode(val)
+    
+    traverse(head)
+    
+    head.next = new_node
+    return new_node
+    
+    
+
+head = ListNode(1)
+
+head = insert_at_head(head, 0)
+                     # 1
+tail = insert_at_tail(head.next, 2)
+
+assert(head.val, head.next.val, head.next.next.val, head.next.next.next) == (0, 1, 2, None)
+    
+    
 
 
 
